46721952d2f95753b9049881354ad2564119637b
mshev <mshev@list.ru>
2019-06-06 21:30:04 +0300

    HW2 in progress

--- HW2/main.go
+++ HW2/main.go
@@ -7,11 +7,25 @@ import (
 	"unicode"
 )
 
+type runeType int
+
+const (
+	symbol runeType = iota
+	count
+	escapeSymbol
+	escape
+)
+const escapeRune rune = '\\'
+
 var input = []string{"a4bc2d5e", "abcd", `qwe\4\5`, `qwe\45`, `qwe\\5`}
 var output = []string{"aaaabccddddde", "abcd", `qwe45`, `qwe44444`, `qwe\\\\\`}
-const escape rune = '\\'
 
 func main() {
+	// fmt.Println("symbol 0 ->", defineRuneType('\\', '4'))
+	// fmt.Println("count 1 -> ", defineRuneType('a', '2'))
+	// fmt.Println("escapeSymbol 2 -> ", defineRuneType('\\', '\\'))
+	// fmt.Println("escape 3 -> ", defineRuneType('4', '\\'))
+
 	for i, v := range input {
 		result := strUnpacker(v)
 
@@ -29,17 +43,20 @@ func strUnpacker(str string) string {
 	runes := []rune(str)
 
 	for i := 0; i < len(runes); i++ {
-		if i > 0 && runes[i] == escape && runes[i - 1] != escape  {
-			continue
-		}
-
-		if unicode.IsDigit(runes[i]) && i > 0 && runes[i - 1] != escape {
-
-			count, _ := strconv.Atoi(string(runes[i]))
-
-			strBuilder.WriteString(multiplyRune(runes[i-1], count))
+		currentRune := runes[i]
+		if i > 0 {
+			prevRune := runes[i-1]
+			symbolType := defineRuneType(prevRune, currentRune)
+			
+			switch {
+			case symbolType == symbol || symbolType == escapeSymbol:
+				strBuilder.WriteRune(currentRune)
+			case symbolType == count:
+				countS, _ := strconv.Atoi(string(runes[i]))
+				strBuilder.WriteString(multiplyRune(prevRune, countS))	
+			}
 		} else {
-			strBuilder.WriteRune(runes[i])
+			strBuilder.WriteRune(currentRune)
 		}
 	}
 
@@ -54,4 +71,19 @@ func multiplyRune(r rune, count int) string {
 	}
 
 	return strBuilder.String()
+}
+
+func defineRuneType(prevRune rune, currentRune rune) runeType {
+	isNumber := unicode.IsDigit(currentRune)
+
+	switch {
+	case prevRune != escapeRune && isNumber:
+		return count
+	case prevRune == escapeRune && currentRune == escapeRune:
+		return escapeSymbol
+	case prevRune != escapeRune && currentRune == escapeRune:
+		return escape
+	default:
+		return symbol
+	}
 }
